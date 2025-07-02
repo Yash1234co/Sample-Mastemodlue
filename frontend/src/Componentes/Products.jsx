c93110142de629050acaa9ae016bb9030fe450e8
yash <yashprajapati7523@gmail.com>
2025-07-02 15:48:50 +0530

    update products code

--- frontend/src/Componentes/Products.jsx
+++ frontend/src/Componentes/Products.jsx
@@ -4,17 +4,19 @@ export default function Sidebar() {
   const navigate = useNavigate();
 
   return (
-    <aside className="bg-gray-400 w-64 h-screen text-black sticky top-0 rounded-md">
-      <div className="flex flex-col px-4 py-6  space-y-4">
+    <aside className="fixed  left-0 w-64 h-screen bg-[#1C2532] text-white shadow-lg z-50 rounded-tr-2xl rounded-br-2xl">
+      <div className="flex flex-col px-4 pt-4">
+        <h2 className="text-xl font-semibold text-center mb-3">Dashboard</h2>
+
         <button
-          className="bg-slate-100 rounded-md px-6 py-2 hover:bg-black hover:text-white transition"
+          className="bg-gray-100 text-[#1C2532] font-semibold rounded-lg px-4 py-2 mb-2 hover:bg-black hover:text-white transition-all duration-200"
           onClick={() => navigate('/products')}
         >
           Products
         </button>
 
         <button
-          className="bg-slate-100 rounded-md px-6 py-2 hover:bg-black hover:text-white transition"
+          className="bg-gray-100 text-[#1C2532] font-semibold rounded-lg px-4 py-2 hover:bg-black hover:text-white transition-all duration-200"
           onClick={() => navigate('/sample')}
         >
           Sample
